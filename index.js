ad2907eb34a97df5eba2562872d88cb845bed3c1
tmh1293 <tmh.101293@gmail.com>
2021-11-16 16:08:21 +0700

    test

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+app.listen(process.env.PORT, (err)=>{
+    if(err) throw err;
+    console.log(`Server running on port ${process.env.PORT}`);
+});
+
+mongoose.connect(process.env.MONGODB_URI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true, 
+    }).then(()=>{
+        console.log("MongoDB Connected");
+    }).catch(err => console.log(err))
